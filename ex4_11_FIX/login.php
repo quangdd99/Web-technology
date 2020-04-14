776d38b0cc11992ea62ab4d95358d022c57f857a
quangdd99 <57699865+quangdd99@users.noreply.github.com>
2020-04-13 17:06:14 -0700

    FIX EX4 11 LOGIN

---
+++ ex4_11_FIX/login.php
@@ -0,0 +1,10 @@
+<?php
+    $username = $_POST["username"];
+    $tel = $_POST["tel"];
+    $mail = $_POST["mail"];
+
+    echo 'LOGIN SUCESSFULL!';
+    echo '<br>You have username: ' . $username; 
+    echo '<br>Telephone: ' . $tel; 
+    echo '<br>And email: ' . $mail; 
+?>
